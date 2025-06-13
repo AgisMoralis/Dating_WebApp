24d274d91b58ff0aa86d21c4c63a9d3fad166b53
AgisMoralis <amoralis94@gmail.com>
2025-06-13 13:09:50 +0200

    - Import "Route" in "Nav" component and inject that service so that it is used when the use logs out or in, to navigate to a specific route/component - Install "ngx-toastr" angular library for displaying nice UI "Toast" messages - Use the "toastr" service to display a "Toast" with the error message when the login failed - Use the "register" service to display a "Toast" with the error message when the registering failed

--- DatingApp/Client/src/app/nav/nav.component.ts
+++ DatingApp/Client/src/app/nav/nav.component.ts
@@ -2,7 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,19 +13,20 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   accountService = inject(AccountService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.error(error)
+      next: () => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error),
     });
   }
   
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
