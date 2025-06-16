0a2519d8a6470b3b6e60c5003618a627d487ae2c
AgisMoralis <amoralis94@gmail.com>
2025-06-16 18:02:05 +0200

    - Remove the old database using "dotnet ef database drop" command from entity framework - Once the application is started from the first time, the "SeedUsersAsync" of the "DatingApp.API.Data.Seed" is executed and the database is populated with the data - Remove the seeding code from the "Program" class

--- DatingApp/DatingApp.API/Program.cs
+++ DatingApp/DatingApp.API/Program.cs
@@ -1,6 +1,5 @@
 using DatingApp.API.Extensions;
 using DatingApp.API.Middleware;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,19 +19,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<DatingApp.API.Data.DataContext>();
-    await context.Database.MigrateAsync();
-    await DatingApp.API.Data.Seed.SeedUsersAsync(context);
-}
-catch (Exception ex)
-{
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occurred during migration");
-}
 
 app.Run();
