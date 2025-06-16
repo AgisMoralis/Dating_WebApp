7627d59a7882ad6770b5388f8b818140f3bdd1d9
AgisMoralis <amoralis94@gmail.com>
2025-06-16 17:36:09 +0200

    - Add new properties into the "User" entity class - Add new "Photo" entity class - Add new extension method that calculates the age of a "User", based on the DateTime property "DateOfBirth" they have - Define the "Photo" entity class properties according to Framework entities conventions, so that the "required one-to-many" navigation properties that define the relationship of the "Photo" to the "User" entity are created - Use "json-generator.com" to generate random data based on our database format to create the "UserSeedData.json" file that will be used to populate the database - Create the static method "SeedUsersAsync" on the new "Seed" class can be used at the startup of our api in the Program class, to seed the data into the database

--- DatingApp/DatingApp.API/Entities/User.cs
+++ DatingApp/DatingApp.API/Entities/User.cs
@@ -1,9 +1,27 @@
+using DatingApp.API.Extensions;
+
 namespace DatingApp.API.Entities;
 
 public class User
 {
     public int Id { get; set; }
     public required string Username { get; set; }
-    public required byte[] PasswordHash { get; set; }
-    public required byte[] PasswordSalt { get; set; }
+    public byte[] PasswordHash { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
+    public DateOnly DateOfBirth { get; set; }
+    public string KnownAs { get; set; } = string.Empty;
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public required string? Gender { get; set; }
+    public string? Introduction { get; set; }
+    public string? Interests { get; set; }
+    public string? LookingFor { get; set; }
+    public required string? City { get; set; }
+    public required string? Country { get; set; }
+    public List<Photo> Photos { get; set; } = new List<Photo>();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
---
+++ DatingApp/DatingApp.API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DatingApp.API.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static int CalculateAge(this DateOnly dateOfBirth)
+    {
+        var today = DateOnly.FromDateTime(DateTime.UtcNow);
+        var age = today.Year - dateOfBirth.Year;
+
+        if (dateOfBirth > today.AddYears(-age))
+        {
+            age--;
+        }
+
+        return age;
+    }
+}
