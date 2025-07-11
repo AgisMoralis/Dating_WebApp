fe6f77bbe08bb09f313ffe994d62e5d1bd8daaeb
AgisMoralis <amoralis94@gmail.com>
2025-07-11 17:30:06 +0200

    - Create the "LogUserActivity" class and register it in the service container, that is executing a custom code after the API action is completed, to update the "LastActive" member field with the current time - Apply this new filter to the global "BaseAPIController", that will be applicable to any controller, so eventually after any action of any controller is executed, the "LastActive" member field is updated - Update the "TokenService" so that the generated token will include the userId and also the username - Update the "ClaimsPrincipalExtensions" by adding the new "GetUserId(this ClaimsPrincipal user)" function that returns the userId extracted from the token - Improve the new "LogUserActivity" class so that it uses the "GetMemberIdAsync" function instead of the "GetUserByUsernameAsync" function that returns along with the users also the Photos (which are not needed wheneve we log the user activity) - Install the TimeAgo library using the Angular CLI command "npm install ngx-timeago and include it inside the services in the "app.config.ts" - Update the HTML template code of the "member-edit" and "member-detail" components to show the proper date format for "last active" and "created" properties

--- DatingApp/DatingApp.API/Controllers/BaseAPIController.cs
+++ DatingApp/DatingApp.API/Controllers/BaseAPIController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers;
 
+[ServiceFilter(typeof(Helpers.LogUserActivity))]
 [ApiController]
 [Route("api/[controller]")]
 public class BaseAPIController : ControllerBase
