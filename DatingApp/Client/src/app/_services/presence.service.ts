223a82bb99656a8b146cb60b62daa35cd78ecf32
AgisMoralis <amoralis94@gmail.com>
2025-08-25 10:54:36 +0200

    - In the Client app installed the SignalR using the command "npm install @microsoft/signalr " - Create the new "presence" service that utilizes the "createHubConnection(user: User)" invoked every time a new member is connected and starts the connection to SignalR "PresenceHub" server (in our API), and the "stopHubConnection()" to send a message to the SignalR "PresenceHub" server when a user is disconnected - Update the "environment" files to include the "hubsUrl" - Update the "account" service so that it invokes the "presence" service whenever a new user is connected or disconnected

---
+++ DatingApp/Client/src/app/_services/presence.service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+import { User } from '../_models/user';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PresenceService {
+  hubsUrl = environment.hubsUrl;
+  private hubConnection?: HubConnection;
+  private toastr = inject(ToastrService);
+
+  createHubConnection(user: User) {
+    this.hubConnection = new HubConnectionBuilder()
+    .withUrl(this.hubsUrl + 'presence', {
+      accessTokenFactory: () => user.token
+    })
+    .withAutomaticReconnect()
+    .build();
+
+    this.hubConnection.start().catch(error => console.log(error));
+
+    this.hubConnection.on('UserIsOnline', username => {
+      this.toastr.info(username + ' has connected');
+    });
+    this.hubConnection.on('UserIsOffline', username => {
+      this.toastr.warning(username + ' has disconnected');
+    });
+  }
+
+  stopHubConnection() {
+    if(this.hubConnection?.state === HubConnectionState.Connected) {
+      this.hubConnection.stop().catch(error => console.log(error));
+    }
+  }
+}
